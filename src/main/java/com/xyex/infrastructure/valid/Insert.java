723bfdab2c60dc30bcd2000453c5a51b63228424
xj <2303_77272132@noreply.gitcode.com>
2025-11-19 10:00:59 +0800

    chore: remove author comments from validation interfaces

--- src/main/java/com/xyex/infrastructure/valid/Insert.java
+++ src/main/java/com/xyex/infrastructure/valid/Insert.java
@@ -1,7 +1,5 @@
 package com.xyex.infrastructure.valid;
 
-/**
- * 插入验证分组
- */
+
 public interface Insert {
 }
--- src/main/java/com/xyex/infrastructure/valid/Update.java
+++ src/main/java/com/xyex/infrastructure/valid/Update.java
@@ -1,7 +1,5 @@
 package com.xyex.infrastructure.valid;
 
-/**
- * 更新验证分组
- */
+
 public interface Update {
 }
